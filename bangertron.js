b46777f4238bed49baf41f10511444dee4a3125a
Chris Davies <chrislewellyn@googlemail.com>
2020-10-26 15:50:45 +0000

    bangertron

---
+++ bangertron.js
@@ -0,0 +1,7 @@
+alert("hello, world!");
+
+// javascript:(function(){ 
+//	var script = document.createElement('script'); 
+//	script.src = "http://localhost:8080/bangertron,js";
+//	document.getElementsByTagName('head')[0].appendChild(script);
+//})();
